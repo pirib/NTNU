849def9750d31f4791873fdfd762fa133992b591
Piri <212310@usn.no>
2021-01-28 10:44:57 +0100

    Nodes and the grid are in place

--- AI Programming/Project1 - Reinforcement Learning/hex_grid.py
+++ AI Programming/Project1 - Reinforcement Learning/hex_grid.py
@@ -12,19 +12,26 @@ class Grid():
 
     # Private
     grid = []
+    
+    # Helper tuples indicating possible neighours' offsets [row, col]
+    offset_triangle = ( (-1,-1) , (0,-1) , (-1,0) , ( 0, 1) , ( 1, 0) , (1, 1) )
+    offset_diamond =  ( ( 0,-1) , (1,-1) , (1, 0) , (-1, 0) , (-1, 1) , (0, 1) )
+
+    offset = ( (-1,-1) , (0,-1) , (-1,0) , ( 0, 1) , ( 1, 0) , (1, 1) )
 
     # Methods
     # Initialize the grid. grid_type - 0 for diamond, 1 for triangle
     # Size is as defined in hex-board-games.pdf
     def __init__(self, grid_type, size):
-        self.make_grid(grid_type, size)
+        self.create(grid_type, size)
         
 
-    def make_grid(self, grid_type, size):
+    def create(self, grid_type, size):
         
-        # Initial, e.g. the first row of the grid - it is the same as size for diamond and 0 (e.g. 1) for the triangle
+        # Initial, e.g. the first row of the grid - it is the same as size for diamond and 0 for the triangle (meaning there will be one "starting" node)
         init_size = size if grid_type == 0 else 0
-                
+            
+        # Creating nodes
         for c in range(size):
             # Create a new empty row
             self.grid.append([])
@@ -35,16 +42,48 @@ class Grid():
             for r in range (init_size):
                 # Make a new node and shove into the right column. The node is then accessible at grid[c][r].
                 self.grid[c].append( N.Node(r,c,size, grid_type) )
-                
-                
-    # Helpers
+
+        # The nodes are generated, so it is time to set its neighbours
+        for row in self.grid:
+            for node in row:
+                for o in self.offset:
+                    
+                    # Potential neighbour coordinates
+                    p_n_r = node.row + o[0]
+                    p_n_c = node.col + o[1]
+                    
+                    # As long as potential coordinates are not negative (since Python allows using negative indexes...)
+                    if p_n_c >= 0 and p_n_r >= 0:
+                        
+                        try:
+                            # TODO for some reason this appends a Node wrapped in a list 
+                            node.neighbours.append( self.grid[p_n_c][p_n_r] )
+                        except:
+                            pass
+                        
+
+    # Debug
+    
+    # Gives a nice little representation of the grid
     def print_grid(self):
         for row in self.grid:
             for node in row:
-                print(node.neighbours)
-                
+                print("*", end = "")
             print()
 
+    # Prints every Node's neigbhours in the grid
+    def print_neighbours(self):
+        for row in test.grid:
+            for n in row:
+                n.print_neighbours()
+        
     
-test = Grid(1,4)
-#test.print_grid()
+    
+    
+    
+test = Grid(0,2)
+test.print_grid()
+
+test.print_neighbours()
+
+
--- AI Programming/Project1 - Reinforcement Learning/node.py
+++ AI Programming/Project1 - Reinforcement Learning/node.py
@@ -12,36 +12,17 @@ class Node:
         self.col = col
         self.row = row
         self.empty = False
-        self.neighbours = []        
+        self.neighbours = list()        
         
-        # Setting neighbours coordinates based on its own col/row and the total size of the grid
-        self.set_neighbours(grid_size, grid_type)
 
     # Set nodes neighbours depending on its placement and total grid_size
-    def set_neighbours(self, grid_size, grid_type):
-
-        # Helper tuples indicating possible neighours' outset
-        offset_triangle = ( (-1,-1) , (0,-1) , (-1,0) , ( 0, 1) , ( 1, 0) , (1, 1) )
-        offset_diamond =  ( ( 0,-1) , (1,-1) , (1, 0) , (-1, 0) , (-1, 1) , (0, 1) )
-                
-        if grid_type == 0 :
+    def set_neighbours(self, neighbours):
+        self.neighbours = neighbours
         
-            # Checking each possible neighbour based on the offset
-            for o in offset_diamond:
-                # If it is a diamond, then it is a piece of cake
-                # For each possible neighbour, e.g. coordinates of the created node + offset check if it stays within the bounds of the grid_size
-                
-                # Potential neighbour coordinates
-                pot_n = [self.row + o[0], self.col + o[1]]
-                
-                if ( 0 <= pot_n[0] < grid_size and 0 <= pot_n[1] < grid_size):
-                    self.neighbours.append( pot_n )        
-        else:
-            for o in offset_diamond:
-                
-                  pot_n = [self.row + o[0], self.col + o[1]]
-                  
-                  if ( 0 <= pot_n[0] < grid_size and 0 <= pot_n[1] <= self.col + 1):
-                    self.neighbours.append( pot_n )
-            
-        print(self.neighbours)  
+    
+    # Debug methods
+    def print_neighbours(self):
+        for n in self.neighbours:
+            print(n.col, n.row, " ", end = "")
+        print()
+
