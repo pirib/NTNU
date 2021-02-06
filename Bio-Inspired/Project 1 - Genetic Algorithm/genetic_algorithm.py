95478ebeb8d1a62c4a471ab7b755d8c6fa31e9a1
Piri <212310@usn.no>
2021-02-06 13:35:13 +0100

    Offspring creation isi finalized

--- Bio-Inspired/Project 1 - Genetic Algorithm/genetic_algorithm.py
+++ Bio-Inspired/Project 1 - Genetic Algorithm/genetic_algorithm.py
@@ -16,6 +16,7 @@ class SGA():
 
     # The list of all individuals
     current_population = []
+    current_offspring = []
 
     # The default values used are the ones that were found to be performance-wise most promising
     
@@ -25,10 +26,11 @@ class SGA():
     # individual_size   - how many bits are used to represent an individual
     # selection_per     - the percentage of population that is selected on the parent selection step. The rest is discarded.
     
-    def __init__(self, population_size = 100, individual_size = 7, selection_per = 50 ):
+    def __init__(self, population_size = 50, individual_size = 7, selection_per = 50 ):
         
         # Clear up the current population
         self.clear_current_population()
+        self.clear_current_offspring()
         
         # Set the population size
         self.population_size = population_size
@@ -57,18 +59,20 @@ class SGA():
         pass
     
             
-
     # Task c
-    # Creates offspring - combines mutation and recombination
-    def create_offspring(self, recombination = True, rec_type = 0, rec_p = 0.5, mutation = True, mut_p = 0.1, mut_bitwise_p = 0.05 ):
-        
-        individual = None
-        
+    # Creates offspring - applies recombination to get offpsring from a set of two parents, then applies mutation
+    def create_offspring(self, selected_parents, recombination = True, rec_type = 0, rec_p = 0.5, mutation = True, mut_p = 0.1, mut_bitwise_p = 0.05 ):
+
+        # Generational genetic algorithm
+        # The entire population will replaced by the offspring                                
+        offspring =[]
+
+        # Apply recombination if valied
         if recombination:
             
-            # TODO: Pick two parents
-            p1 = None
-            p2 = None
+            # Get the two parents for easy access
+            p1 = selected_parents[0]
+            p2 = selected_parents[1]
             
             # One-point crossover
             if rec_type == 0:
@@ -76,23 +80,28 @@ class SGA():
                 # Pick a random point between 0 and len - 1
                 cp = random.randint(0, self.individual_size-1)
                 
+                # Create two kids
                 offspring_1 = p1[:cp] + p2[cp:]
                 offspring_2 = p2[:cp] + p1[cp:]
                 
+                # Pack it into the offspring array
+                offspring.append(offspring_1)
+                offspring.append(offspring_2)
+                
             # Uniform crossover
             elif rec_type == 1:
-                # TODO me next!
-                pass
+                # TODO me next if there is time
+                Exception("Uniform crossovere has not been implemented yet.")
             
             else:
                 Exception("Unrecognized recombination type in offspring creation.")
 
-        # 
-        if mutation:
-            self.mutate( individual, mut_bitwise_p )
-            
-        pass
-    
+        # Mutate the offspring with a probability mut_p
+        if mutation and random.random() < mut_p:
+            for o in offspring:         
+                o = self.mutate( o, mut_bitwise_p )
+                            
+        return offspring
     
     # Task d
     def select_survivors(self):
@@ -104,14 +113,14 @@ class SGA():
         return sin( self.decode(individual) )
                 
     
-    # Simple bit string mutation
+    # Simple bit string mutation - returns a mutated individual
     
     # Iterates through the entire individual, and flips a bit with a chance mut_p
     def mutate(self, individual, mut_p):
         for i in range( len(individual)):
             if random.random() < mut_p:            
                 individual = individual[:i] + str( int( not int(individual[i]))) + individual[i+1:]
-                
+        return individual
                 
     # Analytics and Plotting        
     # =========================================================
@@ -137,6 +146,10 @@ class SGA():
     # Clears up the population array and deletes the individuals
     def clear_current_population(self):
         self.current_population.clear()
+    
+    # Clears up the population array and deletes the individuals
+    def clear_current_offspring(self):
+        self.current_offspring.clear()
     
     # Returns the phenotype of the individual (e.g integer)
     # Expects a bitstring
