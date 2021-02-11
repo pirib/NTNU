ad0aa0fa0a941a1ca859a5c1d51b61ce038cf3d1
Piri <212310@usn.no>
2021-02-11 11:27:38 +0100

    Started with second assignment in AI Methods

---
+++ AI methods/Assignment2/Probabilistic Reasoning over Time.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb  9 14:40:06 2021
+
+@author: babay
+"""
+
+# TASK 1
+
+
+# Evidence
+e1 = True
+e2 = True
+e3 = False
+e4 = True
+e5 = False
+e6 = True
+
+
+# a
+
+
+
